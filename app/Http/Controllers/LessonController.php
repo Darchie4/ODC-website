1aea40ab41f397f0e8d07d857c09ea0df5a330e2
Mads W. Pedersen <peder21@student.sdu.dk>
2023-04-10 22:50:47 +0200

    Lesson creation done, might have to rethink it tho

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Lesson;
+use App\Models\Location;
+use App\Models\Teacher;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\View\View;
@@ -26,17 +28,38 @@ class LessonController extends Controller
      */
     public function create(): View
     {
+        return view('adminPages/lesson/create', ["teachers" => Teacher::all(), "locations" => Location::all()]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return Response
      */
     public function doCreate(Request $request)
     {
+        $request->validate([
+            'name' => 'required',
+            'age' => 'required',
+            'day' => 'required',
+            'start_time' => 'required',
+            'end_time' => 'required',
+            'km_id' => 'required|integer',
+            'teacher' => 'required|integer',
+            'location' => 'required|integer',
+        ]);
 
+        $lesson = new Lesson();
+        $lesson->name = \request("name");
+        $lesson->age = \request("age");
+        $lesson->day = \request("day");
+        $lesson->lesson_start_time = \request("start_time");
+        $lesson->lesson_end_time = \request("end_time");
+        $lesson->km_id = \request("km_id");
+        $lesson->teacher_id = \request("teacher");
+        $lesson->location_id = \request("location");
+        $lesson -> save();
     }
 
     /**
@@ -64,7 +87,7 @@ class LessonController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @param Lesson $lesson
      * @return Response
      */
