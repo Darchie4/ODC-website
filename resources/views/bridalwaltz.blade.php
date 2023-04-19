06816317aee76ee0793f69c2827bc9864ac69ac7
Mads W. Pedersen <peder21@student.sdu.dk>
2023-04-19 10:21:28 +0200

    Brydevals-siden er nu færdig... for nu

--- resources/views/bridalwaltz.blade.php
+++ resources/views/bridalwaltz.blade.php
@@ -16,15 +16,14 @@
     <h1>Brudevals på fyn</h1>
     <article>
         Bor du på fyn og ønsker brudevals undervisning, er Odense Danse Center det rigtige valg.<br>
-        Vi ligger helt rigtigt placeret i Odense, nærmere Dalum, som er tæt på flere motorvejs til/afkørsler.<br>
+        Vi ligger helt rigtigt placeret i Odense, nærmere Dalum, som er tæt på flere motorvejs til/afkørsler.<br><br>
 
-        Vi er eksperter i, at undervise i brudevals, men derfor er vi absolut ikke de dyreste. Tværtimod - skal du have kyndig brudevals undervisning på fyn, så er vi i den billige ende, sammenlignet med andre udbydere på fyn og i Odense.<br>
+        Vi er eksperter i, at undervise i brudevals, men derfor er vi absolut ikke de dyreste. Tværtimod - skal du have kyndig brudevals undervisning på fyn, så er vi i den billige ende, sammenlignet med andre udbydere på fyn og i Odense.<br><br>
 
-        Om I ønsker brudevals undervisning som en klassisk brudevals eller ønsker individuelle trin, så kan vi håndtere det. Vi er vant til at undervise, både brudevals men også de klassiske standard og latin danse kendt fra fx. vild med dans.<br>
+        Om I ønsker brudevals undervisning som en klassisk brudevals eller ønsker individuelle trin, så kan vi håndtere det. Vi er vant til at undervise, både brudevals men også de klassiske standard og latin danse kendt fra fx. vild med dans.<br><br>
 
-        Kontakt os gerne for en uforpligtende snak om netop jeres brudvals undervisning.<br>
-
-        Vi kan kontaktes både telefonisk og via sms på tlf. <a href="tel:+45-42-36-41-15">42 36 41 15 </a> <br><br>
+        <h3>Kontakt os gerne for en uforpligtende snak om netop jeres brudvals undervisning.<br>
+        Vi kan kontaktes både telefonisk og via sms på tlf. <a href="tel:+45-42-36-41-15">42 36 41 15 </a></h3><br><br>
     </article>
 </main>
 
