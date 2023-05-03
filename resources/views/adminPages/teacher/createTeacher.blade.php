9e9ce382be218ee231325ba8d4c9fe6b10f98925
Mads W. Pedersen <peder21@student.sdu.dk>
2023-05-03 18:03:38 +0200

    Loads of optimications for phones

--- resources/views/adminPages/teacher/createTeacher.blade.php
+++ resources/views/adminPages/teacher/createTeacher.blade.php
@@ -27,12 +27,7 @@
             tinymce.init({
                 selector: 'textarea#longDescription',
                 toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-                tinycomments_mode: 'embedded',
-                tinycomments_author: 'Author name',
-                mergetags_list: [
-                    {value: 'First.Name', title: 'First Name'},
-                    {value: 'Email', title: 'Email'},
-                ]
+                tinycomments_mode: 'embedded'
             });
         </script>
         <label for="teacherImg">Billede</label> <br>
