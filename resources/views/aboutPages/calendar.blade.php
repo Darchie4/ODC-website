70a5dea7249f1856ca3430db04b998711b477fe3
Mads W P <mads456dk@gmail.com>
2023-09-24 16:26:50 +0200

    Created controller for "about us"

---
+++ resources/views/aboutPages/calendar.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    @include("partials.metatags")
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="{{ asset('styles/schedule.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('styles/reusables/global.css') }}"/>
+    <script src="{{ asset('js/scheduleHide.js')}}"></script>
+    <script src="https://www.kryogenix.org/code/browser/sorttable/sorttable.js"></script>
+    <title>Odense Danse Center</title>
+</head>
+
+<body>
+@include("partials.navbar")
+
+<main>
+
+    <h1>Kalender</h1>
+
+</main>
+
+@include("partials.footer")
+</body>
+</html>
