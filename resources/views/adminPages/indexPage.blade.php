7f2fb0136e4c3e23f145e9d567e1ff1624216437
Mads W P <mads456dk@gmail.com>
2024-07-25 11:57:47 +0200

    Added links collection to admin homepage

--- resources/views/adminPages/indexPage.blade.php
+++ resources/views/adminPages/indexPage.blade.php
@@ -13,6 +13,16 @@
     <article>
         <h1>Du er nu logget ind som admin</h1>
     </article>
+
+    <article>
+        <h2>Links</h2>
+        <ul>
+            <li><a href="https://odensedansecenter.klub-modul.dk/default.aspx" target="_blank">Klubmodul</a><br><br></li>
+            <li><a href="https://www.simply.com/dk/webmail/" target="_blank">Mail</a><br><br></li>
+            <li><a href="https://www.simply.com/dk/controlpanel/odensedansecenter.dk/admin/" target="_blank">Simply</a><br><br></li>
+            <li><a href="https://search.google.com/search-console/performance/search-analytics?resource_id=sc-domain%3Aodensedansecenter.dk" target="_blank">Google søge statistikker</a><br><br></li>
+        </ul>
+    </article>
 </main>
 
 
